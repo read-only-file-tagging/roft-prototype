ffabf53a34c278b91769467ee5dc4631405a0c20
Valdis <valdisxp1@gmail.com>
2021-02-14 18:59:36 +0200

    simplify Tags.scala - it might be slower but way simpler; optimization will be done later

--- src/main/scala/roft/Tags.scala
+++ src/main/scala/roft/Tags.scala
@@ -2,38 +2,22 @@ package roft
 
 import roft.GenTags.{Path, Tag}
 
-case class Tags(version: String, data: Map[Tag, Set[Path]] = Map.empty) extends ImmutableTags[Tags] {
-  override def +(fileWithTag: (Path, Tag)): Tags = {
-    val (path, tag) = fileWithTag
-    val oldPaths = data.getOrElse(tag, Set.empty)
-    val newPaths = oldPaths + path
-    copy(data = data + (tag -> newPaths))
-  }
-  override def -(fileWithTag: (Path, Tag)): Tags = {
-    val (path, tag) = fileWithTag
-    val oldPaths = data.getOrElse(tag, Set.empty)
-    val newPaths = oldPaths - path
-    if (newPaths.isEmpty) {
-      copy(data = data - tag)
-    } else {
-      copy(data = data + (tag -> newPaths))
-    }
-  }
+case class Tags(version: String, data: Set[(Path, Tag)] = Set.empty) extends ImmutableTags[Tags] {
+  override def +(fileWithTag: (Path, Tag)): Tags = copy(data = data + fileWithTag)
+  override def -(fileWithTag: (Path, Tag)): Tags = copy(data = data - fileWithTag)
   override def withVersion(version: String): Tags = copy(version = version)
 
-  override def allTags: Set[Tag] = data.keySet
-  override def pathsForTag(tag: Tag): Set[Path] = data.getOrElse(tag, Set.empty)
+  override def allTags: Set[Tag] = data.map(_._2)
+  override def pathsForTag(tag: Tag): Set[Path] = data.collect {
+    case (file, `tag`) => file
+  }
   override def tagsForPath(file: Path): Set[Tag] = data.collect {
-    case (tag, paths) if paths(file)
-    => tag
-  }.toSet
+    case (`file`, tag) => tag
+  }
 
-  override def withoutEntries: Tags = copy(data = Map.empty)
+  override def withoutEntries: Tags = copy(data = Set.empty)
 }
 
 object Tags {
-  def apply(version: String, entries: (Path, Tag)*): Tags = {
-    val map = entries.groupBy(_._2).mapValues(_.map(_._1).toSet)
-    new Tags(version, data = map)
-  }
+  def apply(version: String, entries: (Path, Tag)*): Tags = new Tags(version, data = entries.toSet)
 }
